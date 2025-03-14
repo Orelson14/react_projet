d91e409ede7e6447bcc73f3a5ed0868899838d5c
Orelson14 <orelson14@gmail.com>
2025-03-14 11:57:38 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  RouterProviderProps,
+} from "react-router-dom";
 import './App.css';
+import Home from './pages/Home';
+import Contact from './pages/contact';
+import Kitchen from './pages/kitchen';
+import AboutUs from './pages/about_us';
+import Outdoor from './pages/Outdoor';
+import {Living} from './pages/Living';
+import Pagecont1 from './pages/pagecont1';
+
+
+interface ChangeRoute{
+  all: {
+    path: string,
+    element: RouterProviderProps
+  }
+}
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: < Home/>,
+  },
+  {
+    path: "/Contact",
+    element: < Contact/>,
+    
+  },
+  {
+    path: "/Kitchen",
+    element: < Kitchen/>,
+    
+  },
+  {
+    path: "/AboutUs",
+    element: < AboutUs/>,
+  },
+  {
+    path: "/Outdoor",
+    element: < Outdoor/>,
+  },
+  {
+    path: "/Living",
+    element: < Living/>,
+  },
+  {
+    path: "/page1",
+    element: < Pagecont1/>,
+  }
+]);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <React.StrictMode>
+        <RouterProvider router={router} />
+      </React.StrictMode>
+      
     </div>
   );
 }
 
 export default App;
+
